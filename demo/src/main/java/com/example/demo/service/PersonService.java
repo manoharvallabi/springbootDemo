8a12a0356fa5f32521475710e7a7f4a7c6c43379
manoharvallabi <vallabimanoharchowdary789@gmail.com>
2021-10-16 10:01:30 +0530

    first cmd commit

---
+++ demo/src/main/java/com/example/demo/service/PersonService.java
@@ -0,0 +1,50 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.PersonDao;
+import com.example.demo.model.Person;
+
+@Service
+public class PersonService {
+
+	private final PersonDao personDao;
+	
+	@Autowired
+	public PersonService(@Qualifier("fakeDao") PersonDao personDao)
+	{
+		this.personDao=personDao;
+	}
+	
+	public int addPerson(Person person)
+	{
+		return personDao.insertPerson(person);
+	}
+	
+	public List<Person> selectAll()
+	{
+		return personDao.selectAll();
+	}
+	
+	public Optional<Person> findPersonById(UUID id)
+	{
+		return personDao.selectPersonById(id);
+	}
+	
+	
+	public int updatePersonById(UUID id,Person person)
+	{
+		return personDao.UpdatePersonById(id, person);
+	}
+	
+	public int deletePerson(UUID id)
+	{
+		return personDao.deletePerson(id);
+	}
+}
